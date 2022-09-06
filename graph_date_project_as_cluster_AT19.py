03e9af9434a2e419b61d14015c7c31e5f3cae246
Alice0206 <40335091+Alice0206@users.noreply.github.com>
2022-09-06 16:36:44 +0200

    Update graph_date_project_as_cluster_AT19.py

--- graph_date_project_as_cluster_AT19.py
+++ graph_date_project_as_cluster_AT19.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from neo4j import GraphDatabase
 from graphviz import Digraph
@@ -11,15 +6,10 @@ from datetime import datetime
 import numpy as np
 
 
-# In[2]:
-
 
 ### begin config
 # connection to Neo4J database
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
-
-
-# In[3]:
 
 
 ##### colors
@@ -68,11 +58,6 @@ c5_yellow = '#fee090'
 c5_light_blue = '#e0f3f8'
 c5_medium_blue = '#91bfdb'
 c5_dark_blue = '#4575b4'
-
-
-
-
-# In[4]:
 
 
 # Crimson 
@@ -165,10 +150,6 @@ c5_dark_blue = '#4575b4'
 
 
 
-
-
-# Aqua	
-"#00FFFF"
 
 
 # Baby Blue	
@@ -273,9 +254,6 @@ c29 = "#87CEEB"
 
 # Zaffre	
 #0818A8
-
-
-# In[5]:
 
 
 #date_person_as_cluster
@@ -528,9 +506,6 @@ def getActivityDF(tx, dot):
                 dot.attr(rankdir = 'LR')
 
 
-# In[6]:
-
-
 dot = Digraph("G",comment='Query Result')
 dot.attr("graph",margin="0")
 
@@ -573,28 +548,3 @@ file = open("activities.dot","w")
 file.write(dot.source)
 file.close()
 dot.render('test-output/round-table.gv', view=True)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
